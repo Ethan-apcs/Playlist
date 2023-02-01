6adc164e1f128b41f71ad5349d6e617ffb37e738
Nathan Tu <ntu23@students.polytechnic.org>
2023-02-01 08:36:19 -0800

    changes

--- Song.java
+++ Song.java
@@ -14,10 +14,10 @@ public class Song {
      * Constructor-- what information needs to be given to make a Song?
      * How will you handle 'liked' songs? It makes sense for a Song not be 'liked' by default
      */
-    public Song(String myTitle, String myArtist, double myDuration){
+    public Song(String myTitle, String myArtist, String string){
         title = myTitle;
         artist = myArtist;
-        duration = myDuration;
+        duration = double;
     }
 
      /**
